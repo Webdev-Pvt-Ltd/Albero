a0346b94f92f79498a61ffcd1ed0ba56b4cb48ad
Anuj3553 <anujverma3553@gmail.com>
2025-09-05 16:04:13 +0530

    feat: Update Loader and Hero components for responsive design

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -4,7 +4,7 @@ import Logo from '../../assets/images/common/loading.png'
 export default function Loader() {
     return (
         <div className="flex flex-col justify-center items-center min-h-screen bg-black">
-            <div className="w-[30vw] h-[25vh] mb-8">
+            <div className="w-full lg:w-[30vw] h-[25vh] mb-8">
                 <img
                     src={Logo}
                     alt="Loading..."
